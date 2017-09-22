463b71e76121f175cb39deac09fa4c404e1e13b1
zhangqing <977821807@qq.com>
2017-09-22 21:14:56 +0800

    the first spider for bolezaixian use scrapy

---
+++ spiders/ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+import datetime
+import re
+import scrapy
+from scrapy.http import Request
+from urllib import parse
+from scrapy.loader import ItemLoader
+
+from ArticleSpider.items import JobBoleArticleItem, ArticleItemLoader
+from utils.common import get_md5
+
+
+class JobboleSpider(scrapy.Spider):
+    name = 'jobbole'
+    allowed_domains = ['blog.jobbole.com']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
+
+    def parse(self, response):
+        """
+        1.获取文章列表页中的文章url并交给scrapy下载后并进行解析
+        2.获取下一页的url并交给scrapy进行下载， 下载完成后交给parse
+        """
+
+        # 解析列表页所有文章url并交给scrapy下载后并进行解析
+        post_nodes = response.css("#archive .floated-thumb .post-thumb a")
+        for post_node in post_nodes:
+            image_url = post_node.css("img::attr(src)").extract_first("")
+            post_url = post_node.css("::attr(href)").extract_first("")
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": image_url}, callback=self.parse_detail)
+
+        # 提取下一页并交给scrapy进行下载
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_url:
+            yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+
+    def parse_detail(self, response):
+        article_item = JobBoleArticleItem()
+        # 提取文章的具体字段
+        front_image_url = response.meta.get("front_image_url", "")
+        # title = response.css(".entry-header h1::text").extract()[0]
+        # create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].replace('·', '').strip()
+        # praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        # fav_nums = response.css(".bookmark-btn::text").extract()[0]
+        # match_re1 = re.match(".*?(\d+).*", fav_nums)
+        # if match_re1:
+        #     fav_nums = int(match_re1.group(1))
+        # else:
+        #     fav_nums = 0
+        #
+        # comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
+        # match_re2 = re.match(".*?(\d+).*", comment_nums)
+        # if match_re2:
+        #     comment_nums = int(match_re2.group(1))
+        # else:
+        #     comment_nums = 0
+        # content = response.css("div.entry").extract()[0]
+        # tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
+        # tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        # tags = ",".join(tag_list)
+        #
+        #
+        # article_item["url_object_id"] = get_md5(response.url)
+        # article_item["title"] = title
+        # article_item["url"] = response.url
+        #
+        # article_item["create_date"] = create_date
+        # article_item["front_image_url"] = [front_image_url]
+        # article_item["praise_nums"] = praise_nums
+        # article_item["comment_nums"] = comment_nums
+        # article_item["fav_nums"] = fav_nums
+        # article_item["tags"] = tags
+        # article_item["content"] = content
+
+        # 通过item loader 加载item
+        item_loader = ArticleItemLoader(item=JobBoleArticleItem(), response=response)
+        item_loader.add_css("title", ".entry-header h1::text")
+        item_loader.add_value("url", response.url)
+        item_loader.add_value("url_object_id", get_md5(response.url))
+        item_loader.add_css("create_date", "p.entry-meta-hide-on-mobile::text")
+        item_loader.add_value("front_image_url", [front_image_url])
+        item_loader.add_css("praise_nums", ".vote-post-up h10::text")
+        item_loader.add_css("fav_nums", ".bookmark-btn::text")
+        item_loader.add_css("comment_nums", "a[href='#article-comment'] span::text")
+        item_loader.add_css("tags", "p.entry-meta-hide-on-mobile a::text")
+        # item_loader.add_css("content", "div.entry")
+
+        article_item = item_loader.load_item()
+
+
+        yield article_item
+
+
+
+
+
+
+
+
+
+
+
+
+
+
